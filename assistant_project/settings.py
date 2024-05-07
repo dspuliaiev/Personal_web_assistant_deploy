15d815023617cb01657840c2359f2b4d7f2a24f2
AlexVS195 <142892792+AlexVS195@users.noreply.github.com>
2024-05-07 15:44:28 +0300

    added weather forecast

--- assistant_project/settings.py
+++ assistant_project/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'users',
     'notes',
     'files',
+    'weather',
 ]
 
 MIDDLEWARE = [
---
+++ weather/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+import requests
+from .utils import get_weather, get_city_from_user
+from django.views.generic import TemplateView
+
+
+def weather(request):
+    city = request.GET.get('city', 'Kyiv')
+    weather_data = get_weather(city)
+    context = {'weather': weather_data}
+    return render(request, 'weather.html', context)
+
+
+def weather_view(request):
+    return render(request, 'weather.html')
+
+
+class WeatherView(TemplateView):
+    template_name = 'weather.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        city = self.request.GET.get('city', 'Kyiv')  # We get the name of the city from the GET parameter 'city', if not specified, we use 'Kyiv' by default
+        weather_data = get_weather(city)  # We receive weather data for the specified city
+        context['weather'] = weather_data  # Adding weather data to context
+        context['city'] = city  # Adding city name to context
+        return context
